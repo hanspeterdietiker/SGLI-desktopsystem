c7ac6b08bc9835b8a66edcb16678191cc994618d
Hanspeter <hansdietiker2012@gmail.com>
2026-04-27 16:12:18 -0300

    feat<WellCome> implemented icon app

--- src/main/java/org/desktop/system/sgli/sgli/HelloApplication.java
+++ src/main/java/org/desktop/system/sgli/sgli/HelloApplication.java
@@ -3,16 +3,19 @@ package org.desktop.system.sgli.sgli;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    Image icone = new Image(HelloApplication.class.getResourceAsStream("/assets/icon.png"));
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("welcome-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        stage.setTitle("SGLI System - Application");
+        stage.getIcons().add(icone);
         stage.setScene(scene);
         stage.show();
     }
