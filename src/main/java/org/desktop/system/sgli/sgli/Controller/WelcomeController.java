65e322128796fdb2b5e5cac4b0d9ba75059cda5b
Hanspeter <hansdietiker2012@gmail.com>
2026-04-27 17:38:14 -0300

    feat<WelcomeController> Implemented Navigation to hub-view

--- src/main/java/org/desktop/system/sgli/sgli/Controller/WelcomeController.java
+++ src/main/java/org/desktop/system/sgli/sgli/Controller/WelcomeController.java
@@ -1,14 +1,27 @@
 package org.desktop.system.sgli.sgli.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class WelcomeController {
-    @FXML
-    private Label welcomeText;
+
 
     @FXML
-    protected void onWelcomeButton() {
-        welcomeText.setText("Estamos Desenvolvendo!");
+    protected void onClickToHub(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/desktop/system/sgli/sgli/hub-view.fxml"));
+        Parent root = loader.load();
+
+        Scene hubScene = new Scene(root, 1000, 720);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        stage.setScene(hubScene);
+        stage.show();
     }
 }
