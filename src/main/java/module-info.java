e8cfb06946d6221b0aeab535c050eaef3f668fc5
Hanspeter <hansdietiker2012@gmail.com>
2026-05-04 13:29:31 -0300

    chore<module> implemented iTextPdf module to work

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module org.desktop.system.sgli.sgli {
     requires javafx.controls;
     requires javafx.fxml;
     requires jakarta.persistence;
-
+    requires itextpdf;
 
     opens org.desktop.system.sgli.sgli.Controller to javafx.fxml;
     exports org.desktop.system.sgli.sgli;
