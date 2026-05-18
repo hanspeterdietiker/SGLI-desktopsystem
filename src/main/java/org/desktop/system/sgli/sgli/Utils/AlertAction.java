2abad87ee9aa4879db4e81725068b156467162af
Hanspeter <hansdietiker2012@gmail.com>
2026-05-18 19:18:51 -0300

    feat<Confirmation Action> confirmation button implemented in utils actions

--- src/main/java/org/desktop/system/sgli/sgli/Utils/AlertAction.java
+++ src/main/java/org/desktop/system/sgli/sgli/Utils/AlertAction.java
@@ -1,6 +1,7 @@
 package org.desktop.system.sgli.sgli.Utils;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 public class AlertAction extends RuntimeException {
     public static void showAlert(String title, String message) {
@@ -9,5 +10,13 @@ public class AlertAction extends RuntimeException {
         alert.setHeaderText(null);
         alert.setContentText(message);
         alert.showAndWait();
+    }
+
+    public static boolean showConfirmation(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        return alert.showAndWait().orElse(ButtonType.CANCEL) == ButtonType.OK;
     }
 }
