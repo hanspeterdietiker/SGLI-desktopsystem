b237f282264c8ef45f49a9e63ba3a07c9592df31
Hanspeter <hansdietiker2012@gmail.com>
2026-05-22 17:24:07 -0300

    chore<hibernate> implemented hibernate's module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,13 @@ module org.desktop.system.sgli.sgli {
     requires javafx.fxml;
     requires jakarta.persistence;
     requires itextpdf;
+    requires org.hibernate.orm.core;
+    requires org.hibernate.orm.community.dialects;
+    requires org.xerial.sqlitejdbc;
+    requires java.sql;
 
     opens org.desktop.system.sgli.sgli.Controller to javafx.fxml;
+    opens org.desktop.system.sgli.sgli.Entity to org.hibernate.orm.core;
     exports org.desktop.system.sgli.sgli;
     exports org.desktop.system.sgli.sgli.Controller;
     exports org.desktop.system.sgli.sgli.Entity;
